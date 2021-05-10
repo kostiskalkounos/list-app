58cc2e1f5421019fd1263414a45f876d444636d9
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-10 21:32:47 +0200

    add a users list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 };
